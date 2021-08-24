f73a8d186ce57dc960ad7dcf52429899020cb20f
Saiv46 <47699394+Saiv46@users.noreply.github.com>
2021-08-24 23:35:45 +0800

    Added PM2 support

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const App = require('./src/')
 console.log('Initializating...')
 const inst = new App(settings)
 console.log('Starting...')
-inst.start().then(() => console.log('Ready!'))
+inst.start().then(() => (console.log('Ready!'), process.send('ready')))
 
-process.on('SIGTERM', async () => {
+process.on('SIGINT', async () => {
   console.log('Stopping...')
   await inst.stop()
   process.exit(0)
